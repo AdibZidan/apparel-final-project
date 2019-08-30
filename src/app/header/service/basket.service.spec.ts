f68f91af715834438e2ad0f28e7b3c2d41046a40
Adib Zidan <94amsz@gmail.com>
2019-08-30 20:58:03 +0200

    Fixed & implemented basic tests in 'basket.service.spec.ts'

--- src/app/header/service/basket.service.spec.ts
+++ src/app/header/service/basket.service.spec.ts
@@ -1,12 +1,31 @@
-import { TestBed } from '@angular/core/testing';
-
 import { BasketService } from './basket.service';
+import { async, TestBed } from '@angular/core/testing';
+
+import { HttpClientModule } from '@angular/common/http';
 
 describe('BasketService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
 
-  it('should be created', () => {
-    const service: BasketService = TestBed.get(BasketService);
-    expect(service).toBeTruthy();
+  let basketService: BasketService;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    basketService = TestBed.get(BasketService);
   });
+
+  it('Should exist/be defined', () => {
+    expect(basketService)
+      .toBeDefined();
+  });
+
+  it('Should be built/compiled', () => {
+    expect(basketService
+      instanceof BasketService)
+      .toBeTruthy();
+  });
+
 });
