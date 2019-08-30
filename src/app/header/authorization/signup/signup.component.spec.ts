e276cb97771195960941db31fc93abaa1e4a9131
Adib Zidan <94amsz@gmail.com>
2019-08-30 20:04:41 +0200

    Fixed & implemented basic tests in 'signup.component.spec.ts'

--- src/app/header/authorization/signup/signup.component.spec.ts
+++ src/app/header/authorization/signup/signup.component.spec.ts
@@ -1,25 +1,52 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SignupComponent } from './signup.component';
+import { ComponentFixture, async, TestBed } from '@angular/core/testing';
+
+import { DebugElement } from '@angular/core';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatSnackBarModule, MatDialogRef } from '@angular/material';
 
 describe('SignupComponent', () => {
-  let component: SignupComponent;
-  let fixture: ComponentFixture<SignupComponent>;
+
+  let signUpComponent: SignupComponent;
+  let signUpFixture: ComponentFixture<SignupComponent>;
+
+  let debugElement: DebugElement;
+  let htmlElement: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SignupComponent ]
-    })
-    .compileComponents();
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        MatSnackBarModule
+      ],
+      declarations: [SignupComponent],
+      providers: [
+        { provide: MatDialogRef }
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(SignupComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    signUpFixture = TestBed.createComponent(SignupComponent);
+    signUpComponent = signUpFixture.componentInstance;
+
+    debugElement = signUpFixture.debugElement;
+    htmlElement = debugElement.nativeElement;
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('Should exist/be defined', () => {
+    expect(signUpComponent)
+      .toBeDefined();
   });
+
+  it('Should be built/compiled', () => {
+    expect(signUpComponent
+      instanceof SignupComponent)
+      .toBeTruthy();
+  });
+
 });
