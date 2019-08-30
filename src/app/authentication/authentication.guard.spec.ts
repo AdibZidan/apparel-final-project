b3a2ba4b93c39ce7146fbbd4fe4a20079ce2fba7
Adib Zidan <94amsz@gmail.com>
2019-08-30 17:32:31 +0200

    Fixed & implemented basic tests in 'authentication.guard.spec.ts'

--- src/app/authentication/authentication.guard.spec.ts
+++ src/app/authentication/authentication.guard.spec.ts
@@ -1,15 +1,24 @@
-import { TestBed, async, inject } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
 
 import { AuthenticationGuard } from './authentication.guard';
+import { Router } from '@angular/router';
+import { SESSION_STORAGE } from 'angular-webstorage-service';
 
-describe('AuthenticationGuard', () => {
+describe('Authentication Guard', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [AuthenticationGuard]
+      providers: [
+        AuthenticationGuard,
+        { provide: Router },
+        { provide: SESSION_STORAGE }
+      ]
     });
   });
 
-  it('should ...', inject([AuthenticationGuard], (guard: AuthenticationGuard) => {
-    expect(guard).toBeTruthy();
-  }));
+  it('Should be created/defined',
+    inject([AuthenticationGuard],
+      (activationGuard: AuthenticationGuard) => {
+        expect(activationGuard).toBeDefined();
+      }));
+
 });
