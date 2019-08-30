e546faa34a637a5f62b94cb8d5109fceb4efd51b
Adib Zidan <94amsz@gmail.com>
2019-08-30 20:20:00 +0200

    Fixed & implemented basic tests in 'navigation.component.spec.ts'

--- src/app/header/navigation/navigation.component.spec.ts
+++ src/app/header/navigation/navigation.component.spec.ts
@@ -1,25 +1,58 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { NavigationComponent } from './navigation.component';
+import { ComponentFixture, async, TestBed } from '@angular/core/testing';
+
+import { DebugElement } from '@angular/core';
+
+import { SearchComponent } from '../search/search.component';
+import { AuthorizationComponent } from '../authorization/authorization.component';
+import { BasketComponent } from '../basket/basket.component';
+import { MatMenuModule, MatDividerModule, MatListModule, MatDialogModule } from '@angular/material';
+
+import { SESSION_STORAGE } from 'angular-webstorage-service';
 
 describe('NavigationComponent', () => {
-  let component: NavigationComponent;
-  let fixture: ComponentFixture<NavigationComponent>;
+
+  let navigationComponent: NavigationComponent;
+  let navigationFixture: ComponentFixture<NavigationComponent>;
+
+  let debugElement: DebugElement;
+  let htmlElement: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NavigationComponent ]
-    })
-    .compileComponents();
+      imports: [
+        MatMenuModule,
+        MatDividerModule,
+        MatListModule,
+        MatDialogModule
+      ],
+      declarations: [
+        NavigationComponent,
+        SearchComponent,
+        AuthorizationComponent,
+        BasketComponent
+      ],
+      providers: [{ provide: SESSION_STORAGE }]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(NavigationComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    navigationFixture = TestBed.createComponent(NavigationComponent);
+    navigationComponent = navigationFixture.componentInstance;
+
+    debugElement = navigationFixture.debugElement;
+    htmlElement = debugElement.nativeElement;
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('Should exist/be defined', () => {
+    expect(navigationComponent)
+      .toBeDefined();
   });
+
+  it('Should be built/compiled', () => {
+    expect(navigationComponent
+      instanceof NavigationComponent)
+      .toBeTruthy();
+  });
+
 });
