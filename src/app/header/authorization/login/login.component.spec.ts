a71b26ea843c4a0658cf1a3899cde2ee02ab3b61
Adib Zidan <94amsz@gmail.com>
2019-08-30 19:45:50 +0200

    Fixed & implemented basic tests in 'login.component.spec.ts'

--- src/app/header/authorization/login/login.component.spec.ts
+++ src/app/header/authorization/login/login.component.spec.ts
@@ -1,25 +1,57 @@
+import { LoginComponent } from './login.component';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { LoginComponent } from './login.component';
+import { DebugElement } from '@angular/core';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatSnackBarModule, MatDialogRef } from '@angular/material';
+
+import { Router } from '@angular/router';
+import { SESSION_STORAGE } from 'angular-webstorage-service';
 
 describe('LoginComponent', () => {
-  let component: LoginComponent;
-  let fixture: ComponentFixture<LoginComponent>;
+
+  let loginComponent: LoginComponent;
+  let loginFixture: ComponentFixture<LoginComponent>;
+
+  let debugElement: DebugElement;
+  let htmlElement: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
-    })
-    .compileComponents();
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        MatSnackBarModule
+      ],
+      declarations: [LoginComponent],
+      providers: [
+        { provide: Router },
+        { provide: MatDialogRef },
+        { provide: SESSION_STORAGE }
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(LoginComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    loginFixture = TestBed.createComponent(LoginComponent);
+    loginComponent = loginFixture.componentInstance;
+
+    debugElement = loginFixture.debugElement;
+    htmlElement = debugElement.nativeElement;
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('Should exist/be defined', () => {
+    expect(loginComponent)
+      .toBeDefined();
   });
+
+  it('Should be built/compiled', () => {
+    expect(loginComponent
+      instanceof LoginComponent)
+      .toBeTruthy();
+  });
+
 });
