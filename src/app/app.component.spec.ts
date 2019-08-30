17bac333046596c1620be4ed4aac7e12279eca2a
Adib Zidan <94amsz@gmail.com>
2019-08-30 16:35:02 +0200

    Fixed & implemented basic tests in 'app.component.spec.ts'

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,67 @@
-import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { ComponentFixture, async, TestBed } from '@angular/core/testing';
 
-describe('AppComponent', () => {
+import { DebugElement } from '@angular/core';
+
+import { RouterTestingModule } from '@angular/router/testing';
+import { MatMenuModule, MatListModule, MatDialogModule, MAT_DIALOG_DATA } from '@angular/material';
+
+import { HeaderComponent } from './header/header.component';
+import { NavigationComponent } from './header/navigation/navigation.component';
+import { SearchComponent } from './header/search/search.component';
+import { AuthorizationComponent } from './header/authorization/authorization.component';
+import { BasketComponent } from './header/basket/basket.component';
+
+import { SESSION_STORAGE } from 'angular-webstorage-service';
+
+describe('Application Component', () => {
+
+  let applicationComponent: AppComponent;
+  let applicationFixture: ComponentFixture<AppComponent>;
+
+  let debugElement: DebugElement;
+  let htmlElement: HTMLElement;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
+      imports: [
+        RouterTestingModule,
+        MatMenuModule,
+        MatListModule,
+        MatDialogModule
       ],
+      declarations: [
+        AppComponent,
+        HeaderComponent,
+        NavigationComponent,
+        SearchComponent,
+        AuthorizationComponent,
+        BasketComponent
+      ],
+      providers: [
+        { provide: SESSION_STORAGE },
+        { provide: MAT_DIALOG_DATA }
+      ]
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+  beforeEach(() => {
+    applicationFixture = TestBed.createComponent(AppComponent);
+    applicationComponent = applicationFixture.componentInstance;
+
+    debugElement = applicationFixture.debugElement;
+    htmlElement = debugElement.nativeElement;
   });
 
-  it(`should have as title 'apparel'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('apparel');
+  it('Should be created/be defined', () => {
+    expect(applicationComponent)
+      .toBeDefined();
   });
 
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to apparel!');
+  it('Should exist/be compiled', () => {
+    expect(applicationComponent
+      instanceof AppComponent)
+      .toBeTruthy();
   });
+
 });
