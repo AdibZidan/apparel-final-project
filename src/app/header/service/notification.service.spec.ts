e3547c38580eb57ec5ab7a2d5173e0224b9f3d1b
Adib Zidan <94amsz@gmail.com>
2019-08-30 21:08:03 +0200

    Fixed & implemented basic tests in 'notification.service.spec.ts'

--- src/app/header/service/notification.service.spec.ts
+++ src/app/header/service/notification.service.spec.ts
@@ -1,12 +1,31 @@
-import { TestBed } from '@angular/core/testing';
-
 import { NotificationService } from './notification.service';
+import { async, TestBed } from '@angular/core/testing';
 
-describe('NotificationService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+import { MatSnackBarModule } from '@angular/material';
 
-  it('should be created', () => {
-    const service: NotificationService = TestBed.get(NotificationService);
-    expect(service).toBeTruthy();
+describe('Notification Service', () => {
+
+  let notificationService: NotificationService;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [MatSnackBarModule]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    notificationService = TestBed.get(NotificationService);
   });
+
+  it('Should exist/be defined', () => {
+    expect(notificationService)
+      .toBeTruthy();
+  });
+
+  it('Should be built/compiled', () => {
+    expect(notificationService
+      instanceof NotificationService)
+      .toBeTruthy();
+  });
+
 });
