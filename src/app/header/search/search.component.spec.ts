f6443508446f6e7d5d285fad52565ff34e836203
Adib Zidan <94amsz@gmail.com>
2019-08-30 20:23:11 +0200

    Fixed & implemented basic tests in 'search.component.spec.ts'

--- src/app/header/search/search.component.spec.ts
+++ src/app/header/search/search.component.spec.ts
@@ -1,25 +1,43 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SearchComponent } from './search.component';
+import { ComponentFixture, async, TestBed } from '@angular/core/testing';
+
+import { DebugElement } from '@angular/core';
+
+import { MatMenuModule } from '@angular/material';
+
 
 describe('SearchComponent', () => {
-  let component: SearchComponent;
-  let fixture: ComponentFixture<SearchComponent>;
+
+  let searchComponent: SearchComponent;
+  let searchFixture: ComponentFixture<SearchComponent>;
+
+  let debugElement: DebugElement;
+  let htmlElement: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SearchComponent ]
-    })
-    .compileComponents();
+      imports: [MatMenuModule],
+      declarations: [SearchComponent]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(SearchComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    searchFixture = TestBed.createComponent(SearchComponent);
+    searchComponent = searchFixture.componentInstance;
+
+    debugElement = searchFixture.debugElement;
+    htmlElement = debugElement.nativeElement;
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('Should exist/be defined', () => {
+    expect(searchComponent)
+      .toBeDefined();
   });
+
+  it('Should be built/compiled', () => {
+    expect(searchComponent
+      instanceof SearchComponent)
+      .toBeTruthy();
+  });
+
 });
