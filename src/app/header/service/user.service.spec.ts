6535c30675811161062d912c30b5c913ef2d08c6
Adib Zidan <94amsz@gmail.com>
2019-08-30 21:12:23 +0200

    Fixed & implemented basic tests in 'user.service.spec.ts'

--- src/app/header/service/user.service.spec.ts
+++ src/app/header/service/user.service.spec.ts
@@ -1,12 +1,31 @@
-import { TestBed } from '@angular/core/testing';
-
 import { UserService } from './user.service';
+import { async, TestBed } from '@angular/core/testing';
+
+import { HttpClientModule } from '@angular/common/http';
 
 describe('UserService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
 
-  it('should be created', () => {
-    const service: UserService = TestBed.get(UserService);
-    expect(service).toBeTruthy();
+  let userService: UserService;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    userService = TestBed.get(UserService);
   });
+
+  it('Should exist/be defined', () => {
+    expect(userService)
+      .toBeDefined();
+  });
+
+  it('Should be built/compiled', () => {
+    expect(userService
+      instanceof UserService)
+      .toBeTruthy();
+  });
+
 });
