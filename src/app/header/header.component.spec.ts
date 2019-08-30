4254630f7441969273f6f03a5a44e8af68d67c1f
Adib Zidan <94amsz@gmail.com>
2019-08-30 21:32:44 +0200

    Fixed & implemented basic tests in 'header.component.spec.ts'

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -1,25 +1,60 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HeaderComponent } from './header.component';
+import { ComponentFixture, async, TestBed } from '@angular/core/testing';
 
-describe('HeaderComponent', () => {
-  let component: HeaderComponent;
-  let fixture: ComponentFixture<HeaderComponent>;
+import { DebugElement } from '@angular/core';
+
+import { NavigationComponent } from './navigation/navigation.component';
+import { SearchComponent } from './search/search.component';
+import { AuthorizationComponent } from './authorization/authorization.component';
+import { BasketComponent } from './basket/basket.component';
+import { MatMenuModule, MatDividerModule, MatListModule, MatDialogModule } from '@angular/material';
+
+import { SESSION_STORAGE } from 'angular-webstorage-service';
+
+fdescribe('HeaderComponent', () => {
+
+  let headerComponent: HeaderComponent;
+  let headerFixture: ComponentFixture<HeaderComponent>;
+
+  let debugElement: DebugElement;
+  let htmlElement: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
-    })
-    .compileComponents();
+      imports: [
+        MatMenuModule,
+        MatDividerModule,
+        MatListModule,
+        MatDialogModule
+      ],
+      declarations: [
+        HeaderComponent,
+        NavigationComponent,
+        SearchComponent,
+        AuthorizationComponent,
+        BasketComponent
+      ],
+      providers: [{ provide: SESSION_STORAGE }]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HeaderComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    headerFixture = TestBed.createComponent(HeaderComponent);
+    headerComponent = headerFixture.componentInstance;
+
+    debugElement = headerFixture.debugElement;
+    htmlElement = debugElement.nativeElement;
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('Should exist/be defined', () => {
+    expect(headerComponent)
+      .toBeDefined();
   });
+
+  it('Should be built/compiled', () => {
+    expect(headerComponent
+      instanceof HeaderComponent)
+      .toBeTruthy();
+  });
+
 });
