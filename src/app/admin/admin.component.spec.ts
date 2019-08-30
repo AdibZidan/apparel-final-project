67fcf7692715e43c54aac5ac62d20ff438252322
Adib Zidan <94amsz@gmail.com>
2019-08-30 16:56:11 +0200

    Fixed & implemented basic tests in 'admin.component.spec.ts'

--- src/app/admin/admin.component.spec.ts
+++ src/app/admin/admin.component.spec.ts
@@ -1,25 +1,49 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { AdminComponent } from './admin.component';
+import { ComponentFixture, async, TestBed } from '@angular/core/testing';
 
-describe('AdminComponent', () => {
-  let component: AdminComponent;
-  let fixture: ComponentFixture<AdminComponent>;
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatDialogModule, MatSnackBarModule } from '@angular/material';
+import { DebugElement } from '@angular/core';
+
+fdescribe('AdminComponent', () => {
+
+  let adminComponent: AdminComponent;
+  let adminFixture: ComponentFixture<AdminComponent>;
+
+  let debugElement: DebugElement;
+  let htmlElement: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AdminComponent ]
-    })
-    .compileComponents();
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        MatDialogModule,
+        MatSnackBarModule
+      ],
+      declarations: [AdminComponent]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(AdminComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    adminFixture = TestBed.createComponent(AdminComponent);
+    adminComponent = adminFixture.componentInstance;
+
+    debugElement = adminFixture.debugElement;
+    htmlElement = debugElement.nativeElement;
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('Should be created/defined', () => {
+    expect(adminComponent)
+      .toBeDefined();
   });
+
+  it('Should exist/be compiled', () => {
+    expect(adminComponent
+      instanceof AdminComponent)
+      .toBeTruthy();
+  });
+
 });
