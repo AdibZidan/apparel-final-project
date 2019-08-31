8aadcfeb3d14ed25aa595312246ed50049f7faaf
Adib Zidan <94amsz@gmail.com>
2019-08-31 09:58:53 +0200

    Fixed & implemented basic tests in 'main.component.spec.ts'

--- src/app/main/main.component.spec.ts
+++ src/app/main/main.component.spec.ts
@@ -1,25 +1,60 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { MainComponent } from './main.component';
 
+import { ComponentFixture, async, TestBed } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+
+import { HttpClientModule } from '@angular/common/http';
+import { MatSnackBarModule } from '@angular/material';
+
+import { CatalogueComponent } from './catalogue/catalogue/catalogue.component';
+import { ItemComponent } from './item/item/item.component';
+
+import { Router } from '@angular/router';
+import { SESSION_STORAGE } from 'angular-webstorage-service';
+
 describe('MainComponent', () => {
-  let component: MainComponent;
-  let fixture: ComponentFixture<MainComponent>;
+
+  let mainComponent: MainComponent;
+  let mainFixture: ComponentFixture<MainComponent>;
+
+  let debugElement: DebugElement;
+  let htmlElement: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MainComponent ]
-    })
-    .compileComponents();
+      imports: [
+        HttpClientModule,
+        MatSnackBarModule
+      ],
+      declarations: [
+        MainComponent,
+        CatalogueComponent,
+        ItemComponent
+      ],
+      providers: [
+        { provide: Router },
+        { provide: SESSION_STORAGE }
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(MainComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    mainFixture = TestBed.createComponent(MainComponent);
+    mainComponent = mainFixture.componentInstance;
+
+    debugElement = mainFixture.debugElement;
+    htmlElement = debugElement.nativeElement;
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('Should exist/be defined', () => {
+    expect(mainComponent)
+      .toBeDefined();
   });
+
+  it('Should be built/compiled', () => {
+    expect(mainComponent
+      instanceof MainComponent)
+      .toBeTruthy();
+  });
+
 });
