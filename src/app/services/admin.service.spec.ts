f86733d0fec06fb7761f587c478459f26a1b34b6
Adib Zidan <94amsz@gmail.com>
2019-08-31 10:02:03 +0200

    Fixed & implemented basic tests in 'admin.service.spec.ts'

--- src/app/services/admin.service.spec.ts
+++ src/app/services/admin.service.spec.ts
@@ -1,12 +1,31 @@
-import { TestBed } from '@angular/core/testing';
-
 import { AdminService } from './admin.service';
+import { TestBed, async } from '@angular/core/testing';
 
-describe('AdminService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+import { HttpClientModule } from '@angular/common/http';
 
-  it('should be created', () => {
-    const service: AdminService = TestBed.get(AdminService);
-    expect(service).toBeTruthy();
+describe('Admin Service', () => {
+
+  let adminService: AdminService;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    adminService = TestBed.get(AdminService);
   });
+
+  it('Should exist/be defined', () => {
+    expect(adminService)
+      .toBeDefined();
+  });
+
+  it('Should be built/compiled', () => {
+    expect(adminService
+      instanceof AdminService)
+      .toBeTruthy();
+  });
+
 });
