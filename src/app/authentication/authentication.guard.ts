00087d4360a4ab2d929e82a5c5cd82b30950851a
Adib Zidan <94amsz@gmail.com>
2019-08-31 22:04:12 +0200

    Refactored 'authentication.guard.ts'

--- src/app/authentication/authentication.guard.ts
+++ src/app/authentication/authentication.guard.ts
@@ -4,7 +4,6 @@ import { CanActivate, Router } from '@angular/router';
 import { AuthenticationService } from './../services/authentication.service';
 import { SESSION_STORAGE, WebStorageService } from 'angular-webstorage-service';
 
-
 @Injectable({ providedIn: 'root' })
 
 export class AuthenticationGuard implements CanActivate {
@@ -12,13 +11,15 @@ export class AuthenticationGuard implements CanActivate {
   constructor(
     private authenticationService: AuthenticationService,
     private router: Router,
-    @Inject(SESSION_STORAGE) private storage: WebStorageService
-
+    @Inject(SESSION_STORAGE)
+    private storage: WebStorageService
   ) { }
 
-  canActivate(): boolean { return this.isLoggedIn(); }
+  public canActivate(): boolean {
+    return this.isLoggedIn();
+  }
 
-  isLoggedIn() {
+  public isLoggedIn() {
     if (this.authenticationService.isLoggedIn()) {
       return true;
     } else {
