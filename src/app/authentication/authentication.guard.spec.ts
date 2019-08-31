4277295b86c2d0e638e5400687429757c9045eeb
Adib Zidan <94amsz@gmail.com>
2019-08-31 22:03:28 +0200

    Refactored 'authentication.guard.spec.ts'

--- src/app/authentication/authentication.guard.spec.ts
+++ src/app/authentication/authentication.guard.spec.ts
@@ -5,6 +5,7 @@ import { Router } from '@angular/router';
 import { SESSION_STORAGE } from 'angular-webstorage-service';
 
 describe('Authentication Guard', () => {
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
