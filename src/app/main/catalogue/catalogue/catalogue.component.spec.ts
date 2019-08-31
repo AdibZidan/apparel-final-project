78b352250d80dc1b266e1a66dc10d6b9bbf6d41f
Adib Zidan <94amsz@gmail.com>
2019-08-31 09:35:25 +0200

    Fixed & implemented basic tests in 'catalogue.component.spec.ts'

--- src/app/main/catalogue/catalogue/catalogue.component.spec.ts
+++ src/app/main/catalogue/catalogue/catalogue.component.spec.ts
@@ -1,25 +1,59 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CatalogueComponent } from './catalogue.component';
+import { ComponentFixture, async, TestBed } from '@angular/core/testing';
+
+import { DebugElement } from '@angular/core';
+
+import { HttpClientModule } from '@angular/common/http';
+import { MatSnackBarModule } from '@angular/material';
+
+import { ItemComponent } from '../../item/item/item.component';
+
+import { Router } from '@angular/router';
+import { SESSION_STORAGE } from 'angular-webstorage-service';
+
 
 describe('CatalogueComponent', () => {
-  let component: CatalogueComponent;
-  let fixture: ComponentFixture<CatalogueComponent>;
+
+  let catalogueComponent: CatalogueComponent;
+  let catalogueFixture: ComponentFixture<CatalogueComponent>;
+
+  let debugElement: DebugElement;
+  let htmlElement: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CatalogueComponent ]
-    })
-    .compileComponents();
+      imports: [
+        HttpClientModule,
+        MatSnackBarModule
+      ],
+      declarations: [
+        CatalogueComponent,
+        ItemComponent
+      ],
+      providers: [
+        { provide: Router },
+        { provide: SESSION_STORAGE }
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(CatalogueComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    catalogueFixture = TestBed.createComponent(CatalogueComponent);
+    catalogueComponent = catalogueFixture.componentInstance;
+
+    debugElement = catalogueFixture.debugElement;
+    htmlElement = debugElement.nativeElement;
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('Should exist/be defined', () => {
+    expect(catalogueComponent)
+      .toBeDefined();
   });
+
+  it('Should be built/compiled', () => {
+    expect(catalogueComponent
+      instanceof CatalogueComponent)
+      .toBeTruthy();
+  });
+
 });
