03606e1585e84eb4122e756b51f89cbd9ab5ef12
Adib <94amsz@gmail.com>
2019-09-01 22:08:44 +0200

    Updated 'describe' in 'login.service.spec.ts'

--- src/app/header/service/login.service.spec.ts
+++ src/app/header/service/login.service.spec.ts
@@ -3,7 +3,7 @@ import { TestBed, async } from '@angular/core/testing';
 
 import { HttpClientModule } from '@angular/common/http';
 
-describe('Login Service', () => {
+describe('LogIn Service', () => {
 
   let loginService: LoginService;
 
