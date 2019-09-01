fb8e5221bdbe870a1e5e123632352453d0baa2ad
Adib Zidan <94amsz@gmail.com>
2019-09-01 09:32:30 +0200

    Refactored 'search.component.spec.ts'

--- src/app/header/search/search.component.spec.ts
+++ src/app/header/search/search.component.spec.ts
@@ -6,7 +6,7 @@ import { DebugElement } from '@angular/core';
 import { MatMenuModule } from '@angular/material';
 
 
-describe('SearchComponent', () => {
+describe('Search Component', () => {
 
   let searchComponent: SearchComponent;
   let searchFixture: ComponentFixture<SearchComponent>;
