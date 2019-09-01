b3050e455b0fe4026b33a8714de66b634c93ea0b
Adib Zidan <94amsz@gmail.com>
2019-09-01 14:19:49 +0200

    Refactored 'main.service.ts'

--- src/app/main/service/main.service.ts
+++ src/app/main/service/main.service.ts
@@ -1,27 +1,25 @@
 import { Injectable } from '@angular/core';
+
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { tap, retry } from 'rxjs/operators';
 
 import { Observable } from 'rxjs';
 import { ItemForm } from '../../forms/ItemForm';
+import { tap } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 
 export class MainService {
-  // private url: string = 'http://localhost:9010:catalogue';
-  private url: string = 'http://localhost:3000/items';
-  // private url: string = 'http://localhost:9010/catalogue';
-  // private url: string = 'http://localhost:8080/catalogue';
+
+  public url = 'http://localhost:3000/items';
 
   constructor(private httpClient: HttpClient) { }
 
-  getItemsFromBackEnd(): Observable<ItemForm[]> {
+  public getItemsFromBackEnd(): Observable<ItemForm[]> {
     const headers = new HttpHeaders({ 'Access-Control-Allow-Origin': '*' });
 
     return this.httpClient
       .get<ItemForm[]>(this.url, { headers })
-      // .pipe(tap(itemsFromBackEnd => console.log(itemsFromBackEnd)))
-      // .pipe(retry(20))
-      .pipe(tap(() => console.log('Received images from Back-End!')));
+      .pipe(tap(() => console.log('Received images from the Back-End!')));
   }
+
 }
