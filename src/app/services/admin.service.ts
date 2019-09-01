c5874e5bfda5f8ecedce85cefb4458790c6655ec
Adib Zidan <94amsz@gmail.com>
2019-09-01 14:23:00 +0200

    Refactored 'admin.service.ts'

--- src/app/services/admin.service.ts
+++ src/app/services/admin.service.ts
@@ -1,26 +1,20 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+
+import { HttpClient } from '@angular/common/http';
+
 import { ItemForm } from '../forms/ItemForm';
+import { Observable } from 'rxjs';
 
+@Injectable({ providedIn: 'root' })
 
-const httpOptions = {
-  headers: new HttpHeaders({
-    //    'Content-Type': 'application/json'
-  })
-};
-
-@Injectable({
-  providedIn: 'root'
-})
 export class AdminService {
 
-  // private url = 'http://localhost:8080/catalogue/upload';
-  private url = 'http://localhost:3000/items';
+  public url = 'http://localhost:3000/items';
 
   constructor(private httpClient: HttpClient) { }
 
-  sendItemToServer(item: ItemForm): Observable<any> {
-    return this.httpClient.post(this.url, item, httpOptions);
+  public sendItemToServer(item: ItemForm): Observable<any> {
+    return this.httpClient.post(this.url, item);
   }
+
 }
