dd58197752488acd8de2fc8633ee7797ff0512e4
Adib Zidan <94amsz@gmail.com>
2019-09-01 10:03:59 +0200

    Refactored 'basket.service.ts'

--- src/app/header/service/basket.service.ts
+++ src/app/header/service/basket.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
+
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { User } from './../../services/user';
-import { ItemForm } from '../../forms/ItemForm';
+
 import { ReserveForm } from 'src/app/forms/ReserveForm';
+import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 
@@ -15,21 +15,21 @@ export class BasketService {
   //   const url: string = `http://localhost:8080/userbasket/${user.userUuId}`;
   //   return this.httpClient.get<User>(url);
   // }
-  
-  reserveItem(reserveForm: ReserveForm): Observable<any> {
-    const itemsUrl: string = 'http://localhost:3000/items';
-    return this.httpClient.put<any>(itemsUrl, reserveForm);
+
+  reserveItem(reserveForm: ReserveForm): Observable<ReserveForm> {
+    const itemsUrl = 'http://localhost:3000/items';
+
+    return this.httpClient.put<ReserveForm>(itemsUrl, reserveForm);
   }
 
   // toggleItem(item: ItemForm): Observable<ItemForm> {
-  //   const url: string = `http://localhost:8080/catalogue/book/${item.id}`
+  //   const url = `http://localhost:8080/catalogue/book/${item.id}`
 
   //   return this.httpClient.put<ItemForm>(url, item);
   // }
 
-  // This method is for Node Back-End 'Testing purposes'
   // toggleCompleted(item: ItemForm): Observable<any> {
-  //   const url: string = `http://localhost:3000/tasks/${item.id}`;
+  //   const url = `http://localhost:3000/tasks/${item.id}`;
 
   //   return this.httpClient.put<ItemForm>(url, item);
   // }
