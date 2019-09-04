e5192e279731abd07f071b758ae2fea9f919031e
Adib Zidan <94amsz@gmail.com>
2019-09-04 20:05:24 +0200

    Added 'describe' block & 'configuration' property test in 'notification.service.spec.ts'

--- src/app/header/service/notification.service.spec.ts
+++ src/app/header/service/notification.service.spec.ts
@@ -1,7 +1,7 @@
 import { NotificationService } from './notification.service';
 import { async, TestBed } from '@angular/core/testing';
 
-import { MatSnackBarModule } from '@angular/material';
+import { MatSnackBarModule, MatSnackBarConfig } from '@angular/material';
 
 describe('Notification Service', () => {
 
@@ -26,6 +26,21 @@ describe('Notification Service', () => {
     expect(notificationService
       instanceof NotificationService)
       .toBeTruthy();
+  });
+
+  describe('Notification Service Properties', () => {
+
+    it(`Should have a defined 'configuration' property`, () => {
+      const expectedConfigurationProperty: MatSnackBarConfig = {
+        verticalPosition: 'bottom',
+        duration: 2000,
+        panelClass: ['white-snackbar']
+      };
+      const actualConfigurationProperty = notificationService.configuration;
+
+      expect(actualConfigurationProperty).toEqual(expectedConfigurationProperty);
+    });
+
   });
 
 });
