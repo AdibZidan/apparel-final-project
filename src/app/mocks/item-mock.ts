0ead6546ad3aa58de84cf6ea98a871365fef89cc
Adib Zidan <94amsz@gmail.com>
2019-09-15 19:18:41 +0200

    Created 'item-mock.ts' in the 'mocks' folder

---
+++ src/app/mocks/item-mock.ts
@@ -0,0 +1,17 @@
+import { ItemForm } from '../forms/ItemForm';
+
+export const itemMock: ItemForm = {
+  id: '1',
+  uuid: '1',
+  userUuId: '1',
+  name: 'Item test',
+  description: 'Description test',
+  imageFilepath: 'Image file-path test',
+  imageUrl: 'Image URL test',
+  gender: 'Gender test',
+  type: 'Type test',
+  color: 'Color test',
+  season: 'Season test',
+  size: 'Size test',
+  booked: true
+};
