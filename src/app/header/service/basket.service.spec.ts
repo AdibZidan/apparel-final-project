8538c2dbf7447bd15300e855a26b70f38d2c70e3
Adib Zidan <94amsz@gmail.com>
2019-09-21 16:31:52 +0200

    Added 'reserveItem' test in 'basket.service.spec.ts'

--- src/app/header/service/basket.service.spec.ts
+++ src/app/header/service/basket.service.spec.ts
@@ -1,21 +1,28 @@
 import { BasketService } from './basket.service';
 import { async, TestBed } from '@angular/core/testing';
 
-import { HttpClientModule } from '@angular/common/http';
+import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
+
+import { reservedItemMock } from 'src/app/mocks/reserved-item-mock';
+import { ReserveForm } from 'src/app/forms/ReserveForm';
 
 describe('Basket Service', () => {
 
   let basketService: BasketService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientModule]
+      imports: [HttpClientTestingModule],
     }).compileComponents();
   }));
 
   beforeEach(() => {
     basketService = TestBed.get(BasketService);
+    httpTestingController = TestBed.get(HttpTestingController);
   });
+
+  afterEach(() => httpTestingController.verify());
 
   it('Should exist/be defined', () => {
     expect(basketService)
@@ -26,6 +33,22 @@ describe('Basket Service', () => {
     expect(basketService
       instanceof BasketService)
       .toBeTruthy();
+  });
+
+  it(`Should reserveItem via 'PUT' request`, () => {
+    basketService
+      .reserveItem(reservedItemMock)
+      .subscribe((itemToReserve: ReserveForm) => {
+        expect(itemToReserve).toEqual(reservedItemMock);
+      });
+
+    const itemsUrl = 'http://localhost:3000/items';
+    const request = httpTestingController.expectOne(itemsUrl);
+    const method = request.request.method;
+
+    expect(method).toEqual('PUT');
+
+    request.flush(reservedItemMock);
   });
 
 });
