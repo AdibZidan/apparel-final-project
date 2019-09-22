920ebf43ff9eb992ad22c864d9866f02649d0469
Adib Zidan <94amsz@gmail.com>
2019-09-22 10:40:57 +0200

    Instantiated 'httpTestingController' & added 'afterEach' block in 'login.service.spec.ts'

--- src/app/header/service/login.service.spec.ts
+++ src/app/header/service/login.service.spec.ts
@@ -1,21 +1,25 @@
 import { LoginService } from './login.service';
 import { TestBed, async } from '@angular/core/testing';
 
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('LogIn Service', () => {
 
   let loginService: LoginService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientModule]
+      imports: [HttpClientTestingModule]
     }).compileComponents();
   }));
 
   beforeEach(() => {
     loginService = TestBed.get(LoginService);
+    httpTestingController = TestBed.get(HttpTestingController);
   });
+
+  afterEach(() => httpTestingController.verify());
 
   it('Should exist/be defined', () => {
     expect(loginService)
