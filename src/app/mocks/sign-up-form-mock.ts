7a9936733b8d5eef88bc0bd745e45d746392d901
Adib Zidan <94amsz@gmail.com>
2019-09-23 20:48:15 +0200

    Created 'sign-up-form-mock.ts'

---
+++ src/app/mocks/sign-up-form-mock.ts
@@ -0,0 +1,12 @@
+import { SignUpForm } from '../forms/SignUpForm';
+
+export const signUpFormMock: SignUpForm = {
+  userUuId: 'Test',
+  username: 'Test',
+  password: 'Test',
+  matchingPassword: 'Test',
+  email: 'Test',
+  address: 'Test',
+  authority: 'Test',
+  basket: []
+};
